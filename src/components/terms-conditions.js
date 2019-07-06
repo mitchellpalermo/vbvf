0ace56326296d281a61c7e81367d5d5b845a38c6
Mitch <mitchellpalermo@gmail.com>
2019-07-05 20:35:42 -0500

    added terms and conditions page.

--- src/components/terms-conditions.js
+++ src/components/terms-conditions.js
@@ -1,7 +1,191 @@
 import React from "react";
+import "../css/terms-conditions.css";
 
 const TermsConditions = () => {
-  return <div>Terms and Conditions</div>;
+  return (
+    <div className="disclosures">
+      <h1>Terms and Conditions</h1>
+      <h2>Introduction</h2>
+      <p>
+        The Website Owner, Verse By Verse Fellowship (VBVF), including
+        subsidiaries and affiliates (“Website” or “Website Owner” or “we” or
+        “us” or “our”) provides the information contained on this website or any
+        of the pages comprising the website (“website”) to visitors (“visitors”)
+        (cumulatively referred to as “you” or “your” hereinafter) subject to the
+        terms and conditions set out in these website terms and conditions, the
+        privacy policy and any other relevant terms and conditions, policies and
+        notices which may be applicable to a specific section or module of this
+        website. Your use of this website constitutes your agreement with these
+        terms and conditions.
+      </p>
+      <h2>1. Content</h2>
+      <h3>1.1 Information on the website</h3>
+      <p>
+        While every effort is made to update the information contained on this
+        website, neither the Website Owner nor any third party or data or
+        content provider make any representations or warranties, whether
+        express, implied in law or residual, as to the sequence, accuracy,
+        completeness or reliability of information, opinions, research
+        information, data and/or content contained on the website (including but
+        not limited to any information which may be provided by any third party
+        or data or content providers) (“information”) and shall not be bound in
+        any manner by any information contained on the website. The Website
+        Owner reserves the right at any time to change or discontinue without
+        notice, any aspect or feature of this website. No information shall be
+        construed as advice and information is offered for information purposes
+        only and is not intended for counseling or other professional services.
+        You rely on the information contained on this website at your own risk.
+        If you find an error or omission at this site, please let us know.
+      </p>
+      <h3>1.2 Trade Marks</h3>
+      <p>
+        The trade marks, names, logos, text and audio files (collectively “trade
+        marks”) displayed on or downloaded from this website are copyrighted
+        works and/or registered and unregistered trade marks of the Website
+        Owner. Nothing contained on this website should be construed as granting
+        any licence or right to use any trade mark without the prior written
+        permission of the Website Owner.
+      </p>
+      <h3>1.3 External Links</h3>
+      <p>
+        External links may be provided for your convenience, but they are beyond
+        the control of the Website Owner and no representation is made as to
+        their content. Use or reliance on any external links and the content
+        thereon provided is at your own risk. When visiting external links you
+        must refer to that external websites terms and conditions of use. No
+        hypertext links shall be created from any website controlled by you or
+        otherwise to this website without the express prior written permission
+        of the Website Owner. Please contact us if you would like to link to
+        this website or would like to request a link to your website.
+      </p>
+      <h3>1.4 Specific Use</h3>
+      <p>
+        You further agree not to use the website to send or post any message or
+        material that is unlawful, harassing, defamatory, abusive, indecent,
+        threatening, harmful, vulgar, obscene, sexually orientated, racially
+        offensive, profane, pornographic or violates any applicable law and you
+        hereby indemnify the Website Owner against any loss, liability, damage
+        or expense of whatever nature which the Website Owner or any third party
+        may suffer which is caused by or attributable to, whether directly or
+        indirectly, your use of the website to send or post any such message or
+        material.
+      </p>
+      <h3>1.5 Warranties</h3>
+      <p>
+        The Website Owner makes no warranties, representations, statements or
+        guarantees (whether express, implied in law or residual) regarding the
+        website, the information contained on the website, your or your
+        company’s personal information or material and information transmitted
+        over our system.
+      </p>
+      <h3>1.6 Disclaimer of Liability</h3>
+      <p>
+        The Website Owner shall not be responsible for and disclaims all
+        liability for any loss, liability, damage (whether direct, indirect or
+        consequential), personal injury or expense of any nature whatsoever
+        which may be suffered by you or any third party (including your
+        company), as a result of or which may be attributable, directly or
+        indirectly, to your access and use of the website, any information
+        contained on the website, your or your company’s personal information or
+        material and information transmitted over our system. In particular,
+        neither the Website Owner nor any third party or data or content
+        provider shall be liable in any way to you or to any other person, firm
+        or corporation whatsoever for any loss, liability, damage (whether
+        direct or consequential), personal injury or expense of any nature
+        whatsoever arising from any delays, inaccuracies, errors in, or omission
+        of any information or the transmission thereof, or for any actions taken
+        in reliance thereon or occasioned thereby or by reason of
+        non-performance or interruption, or termination thereof.
+      </p>
+      <h3>1.7 Use of the website</h3>
+      <p>
+        The Website Owner does not make any warranty or representation that
+        information on the website is appropriate for use in any jurisdiction
+        (other than the United States). By accessing the website, you warrant
+        and represent to the Website Owner that you are legally entitled to do
+        so and to make use of information made available via the website.
+      </p>
+      <h2>General</h2>
+      <h3>2.1 Entire Agreement</h3>
+      <p>
+        These website terms and conditions constitute the sole record of the
+        agreement between you and the Website Owner in relation to your use of
+        the website. Neither you nor the Website Owner shall be bound by any
+        express tacit or implied representation, warranty, promise or the like
+        not recorded herein. Unless otherwise specifically stated these website
+        terms and conditions supersede and replace all prior commitments,
+        undertakings or representations, whether written or oral, between you
+        and the Website Owner in respect of your use of the website.
+      </p>
+      <h3>2.2 Alteration</h3>
+      <p>
+        The Website Owner may at any time modify any relevant terms and
+        conditions, policies or notices. You acknowledge that by visiting the
+        website from time to time, you shall become bound to the current version
+        of the relevant terms and conditions (the “current version”) and, unless
+        stated in the current version, all previous versions shall be superseded
+        by the current version. You shall be responsible for reviewing the then
+        current version each time you visit the website.
+      </p>
+      <h3>2.3 Conflict</h3>
+      <p>
+        Where any conflict or contradiction appears between the provisions of
+        these website terms and conditions and any other relevant terms and
+        conditions, policies or notices, the other relevant terms and
+        conditions, policies or notices which relate specifically to a
+        particular section or module of the website shall prevail in respect of
+        your use of the relevant section or module of the website.
+      </p>
+      <h3>2.4 Waiver</h3>
+      <p>
+        No indulgence or extension of time which either you or the Website Owner
+        may grant to the other will constitute a waiver of or, whether by
+        estoppel or otherwise, limit any of the existing or future rights of the
+        grantor in terms hereof, save in the event or to the extent that the
+        grantor has signed a written document expressly waiving or limiting such
+        rights.
+      </p>
+      <h3>2.5 Cession</h3>
+      <p>
+        The Website Owner shall be entitled to cede, assign and delegate all or
+        any of its rights and obligations in terms of any relevant terms and
+        conditions, policies and notices to any third party.
+      </p>
+      <h3>2.6 Severability</h3>
+      <p>
+        All provisions of any relevant terms and conditions, policies and
+        notices are, notwithstanding the manner in which they have been grouped
+        together or linked grammatically, severable from each other. Any
+        provision of any relevant terms and conditions, policies and notices,
+        which is or becomes unenforceable in any jurisdiction, whether due to
+        voidness, invalidity, illegality, unlawfulness or for any reason
+        whatever, shall, in such jurisdiction only and only to the extent that
+        it is so unenforceable, be treated as pro non scripto and the remaining
+        provisions of any relevant terms and conditions, policies and notices
+        shall remain in full force and effect
+      </p>
+      <h3>2.7 Applicable Laws</h3>
+      <p>
+        Any relevant terms and conditions, policies and notices shall be
+        governed by and construed in accordance with the laws of the state of
+        Texas without giving effect to any principles of conflict of law. You
+        hereby consent to the exclusive jurisdiction of the state of Texas in
+        respect of any disputes arising in connection with the website, or any
+        relevant terms and conditions, policies and notices or any matter
+        related to or in connection therewith.
+      </p>
+      <h3>2.8 Comments or Questions</h3>
+      <p>
+        If you have any questions, comments or concerns arising from the
+        website, the privacy policy or any other relevant terms and conditions,
+        policies and notices or the way in which we are handling your personal
+        information please
+        <a className="contact-link" href="mailto:info@vbvf.org">
+          contact us.
+        </a>
+      </p>
+    </div>
+  );
 };
 
 export default TermsConditions;
