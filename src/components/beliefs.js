0e6d4c1eeeae6b2b036b3081b26ea31147ac6b73
Mitch <mitchellpalermo@gmail.com>
2019-07-14 19:17:50 -0500

    using useState hook to change components on page. formatted side menu. added header element with photo and h1

---
+++ src/components/beliefs.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Beliefs() {
+  return (
+    <div>
+      <div />
+      <h1>
+        A reliance on scripture.
+        <h3>(2 Peter 1:3)</h3>
+      </h1>
+    </div>
+  );
+}
--- src/containers/about.js
+++ src/containers/about.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import OurStory from "../components/our-story";
+import Beliefs from "../components/beliefs";
+import Leadership from "../components/leadership";
+import FAQ from "../components/faq";
+import "../css/about.css";
 
 const About = () => {
-  return <div>About</div>;
+  const [component, setComponent] = useState(<OurStory />);
+
+  const handleClick = nextComponent => {
+    setComponent(nextComponent);
+  };
+  return (
+    <div>
+      <div className="header-container">
+        <h1>About</h1>
+      </div>
+      <div className="about-container">
+        <div className="side-menu">
+          <ul>
+            <li>
+              <a onClick={() => setComponent(<OurStory />)}>Our Story</a>
+            </li>
+            <li>
+              <a onClick={() => setComponent(<Beliefs />)}>
+                Core Values & Beliefs
+              </a>
+            </li>
+            <li>
+              <a onClick={() => setComponent(<Leadership />)}>Leadership</a>
+            </li>
+            <li>
+              <a onClick={() => setComponent(<FAQ />)}>FAQ</a>
+            </li>
+          </ul>
+        </div>
+        <div className="display-component">{component}</div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
