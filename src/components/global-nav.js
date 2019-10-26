2dfa6122ed829decbe9c3f79d45cc8420eec3abd
Mitch <mitchellpalermo@gmail.com>
2019-10-25 20:00:06 -0500

    created components for each of the ministries, added vbvf logo to nav bar, added routes to app file

---
+++ src/components/global-nav.js
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
+import { Link } from "react-router-dom";
+
+import "../css/global-nav.css";
+import MenuIcon from "../images/logos/vbvf_icon.png";
+
+const GlobalNav = props => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar className="menu-bar" color="light" light expand="md">
+        <NavbarBrand href="/">
+          <img id="menu-bar-icon" src={MenuIcon} alt="VBVF Logo" />
+        </NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/service-time-directions/">
+                Service Times & Directions
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/about">About</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Ministries
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem tag={Link} to="/ministries/childrens-ministry">
+                  Children's Ministry
+                </DropdownItem>
+                <DropdownItem>
+                  <NavItem>
+                    <NavLink
+                      className="sub-menu"
+                      href="/ministries/youth-ministry"
+                    >
+                      Youth Ministry
+                    </NavLink>
+                  </NavItem>
+                </DropdownItem>
+                <DropdownItem>
+                  <NavLink className="sub-menu" href="/small-groups">
+                    Small Groups
+                  </NavLink>
+                </DropdownItem>
+                <DropdownItem>
+                  <NavLink className="sub-menu" href="/serve">
+                    Serving at VBVF
+                  </NavLink>
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+            <NavItem>
+              <NavLink href="https://vbvf.churchcenter.com/registrations/events">
+                Events
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://vbvf.churchcenter.com/giving">
+                Give
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/sermon-redirect">Sermons</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default GlobalNav;
