b36337df12b543252dd90dc607e315d568baaa3f
Mitch <mitchellpalermo@gmail.com>
2019-12-31 17:08:18 -0600

    began styling for mobile view on serve page. finished out image selection on desktop view

--- src/components/scripture-verse.js
+++ src/components/scripture-verse.js
@@ -3,7 +3,7 @@ import "../css/scripture-verse.scss";
 
 export default function ScriptureVerse(props) {
   return (
-    <div>
+    <div className="scripture-container">
       <h3 className="scripture-verse">{props.verse}</h3>
       <h3 className="scripture-reference">{props.reference}</h3>
     </div>
