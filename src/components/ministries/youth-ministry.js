c0bb0e8d1c9bf0be7ff2f0b376b3126cc9d800b3
Mitch <mitchellpalermo@gmail.com>
2020-03-05 20:37:10 -0600

    redoing the youth page to match comp in dropbox

--- src/components/ministries/youth-ministry.js
+++ src/components/ministries/youth-ministry.js
@@ -1,10 +1,9 @@
 import React from "react";
-import LogosLogo from "../../images/logos/logos-logo-white.png";
-import BibleProject from "../../images/logos/bible-project.png";
+import LogosLogo from "../../images/logos/logos-logo.png";
 import "../../css/youth-ministry.scss";
-import InstagramIcon from "../../images/logos/instagram_logo.png";
 import YouthImage from "../../images/youth_ministry_header.jpg";
 import Content from "../../content/content.json";
+import ScriptureVerse from "../scripture-verse";
 
 export default function YouthMinistry() {
   const youthPage = Content.youthPage;
@@ -18,35 +17,31 @@ export default function YouthMinistry() {
           alt="Logos Student Ministry Logo"
         />
       </div>
-
-      <div className="youth-description-container">
-        <div className="words-section">
-          <h2>{youthPage.firstRow.title}</h2>
-          <p>{youthPage.firstRow.body}</p>
-        </div>
-        <img src={YouthImage} alt="youth playing gaga ball" />
-      </div>
-      <div className="bible-project-container">
-        <img src={BibleProject} />
-        <div className="words-section">
-          <h2>{youthPage.secondRow.title}</h2>
-          <p>{youthPage.secondRow.body}</p>
-        </div>
+      <div className="instablocks">
+        <span />
+        <span />
+        <span />
+        <span />
       </div>
       <div className="time-section">
         <h4>{youthPage.thirdRow.title}</h4>
         <p>{youthPage.thirdRow.body}</p>
       </div>
-      <div className="social-media-section">
-        <div className="social-media-tag">
-          <img src={InstagramIcon} />
-          Logos Youth Ministry
+      <div className="description-container">
+        <div className="half-box">
+          <ScriptureVerse
+            verse={youthPage.scriptureVerse.verse}
+            reference={youthPage.scriptureVerse.reference}
+          />
         </div>
-        <div className="instablocks">
-          <span />
-          <span />
-          <span />
-        </div>
+        <p>{youthPage.firstRow.body}</p>
+      </div>
+      <div className="leader-photos">
+        <img></img>
+        <img></img>
+        <img></img>
+        <img></img>
+        <img></img>
       </div>
     </div>
   );
