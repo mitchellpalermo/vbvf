1ad3159354f63d1db9df4de124264147e305259c
Mitch <mitchellpalermo@gmail.com>
2020-07-08 09:11:45 -0500

    putting together a better 404 page with links

--- src/containers/notfound.js
+++ src/containers/notfound.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../css/not-found.scss";
 
 const NotFound = () => {
-  return <div>Uh oh, you're lost.</div>;
+  return (
+    <div className="not-found">
+      <h1>404</h1>
+      <h3>
+        This means that we couldn't find what you're looking for. Sorry about
+        that.
+      </h3>
+      <p>
+        These are some of the most popular pages on our site, maybe you were
+        looking for one of these?
+      </p>
+      <ul>
+        <li>
+          <Link to="/bible-studies/">Bible study media</Link>
+        </li>
+        <li>
+          <Link to="about/faq">FAQ</Link>
+        </li>
+        <li>
+          <Link to="/livestream">Livestream</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default NotFound;
