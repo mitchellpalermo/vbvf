2c8546a2b839c969f65bf350035654d2a0a3419d
Mitch <mitchellpalermo@gmail.com>
2020-07-10 17:48:31 -0500

    added sanity, styling, and more

---
+++ src/containers/childrens-content-aggregator.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+
+import "../css/childrens-content-aggregator.scss";
+import { Link } from "react-router-dom";
+import { Card, CardTitle, CardSubtitle } from "reactstrap";
+import { sanity } from "../util/index";
+
+export default function ChildrensContentAggregator() {
+  const query = `*[_type == "childrensUnit"] {
+    unitNumber,
+    subtitle
+  }`;
+  const [childrensUnits, setChildrensUnits] = useState([]);
+
+  useEffect(() => {
+    sanity.fetch(query).then((results) => setChildrensUnits(results));
+  }, []);
+  console.log(childrensUnits);
+  return (
+    <div className="units">
+      <div className="units-desc">
+        <h1>Children's Teaching Materials</h1>
+      </div>
+      {childrensUnits.map((unit) => (
+        <Link
+          key={unit.unitNumber}
+          className="unit-link"
+          to={`/childrens-content/unit-${unit.unitNumber}`}
+        >
+          <div className="unit-icon">
+            <Card body>
+              <CardTitle>{`Unit ${unit.unitNumber}`}</CardTitle>
+              <CardSubtitle>{unit.subtitle}</CardSubtitle>
+            </Card>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+}
