fe5217358d6fb0d794ba092035880fba579363ee
Mitch <mitchellpalermo@gmail.com>
2020-10-17 10:44:32 -0500

    adding modules to small groups page.

--- src/components/ministries/small-groups.js
+++ src/components/ministries/small-groups.js
@@ -4,87 +4,68 @@ import "../../css/small-groups.scss";
 import Matthew from "../../images/leadership_photos/Matthew_McWaters.jpeg";
 import StaffInfo from "../staff-info";
 import Logo from "../../images/logos/small_group_logo.svg";
-
+import { Spinner } from "reactstrap";
 import MobileMap from "./maps/mobile-map";
 import DesktopMap from "./maps/desktop-map";
 import { getMobileOperatingSystem } from "../../util/index";
 
-import content from "../../content/small-groups-ministry-content";
+require("dotenv").config();
 
 export default function SmallGroups() {
   const [groups, setGroups] = useState();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    axios
-      .get(
+    const options = {
+      url:
         "https://api.planningcenteronline.com/groups/v2/group_types/122121/groups",
-        {
-          Authorization: `Basic ${process.env.PLANNING_CENTER_ENCODED}`,
-        }
-      )
-      .then((response) => {
-        console.log(response);
-        setGroups(response);
-        console.log(groups);
-      });
-
-    // var createCORSRequest = function (method, url) {
-    //   var xhr = new XMLHttpRequest();
-    //   if ("withCredentials" in xhr) {
-    //     // Most browsers.
-    //     xhr.open(method, url, true);
-    //   } else {
-    //     // CORS not supported.
-    //     xhr = null;
-    //   }
-    //   return xhr;
-    // };
-
-    // var url =
-    //   "https://api.planningcenteronline.com/groups/v2/group_types/122121/groups";
-    // var method = "GET";
-    // var xhr = createCORSRequest(method, url);
-
-    // xhr.onload = function () {
-    //   // Success code goes here.
-    // };
-
-    // xhr.onerror = function () {
-    //   // Error code goes here.
-    // };
-
-    // xhr.setRequestHeader(
-    //   "Authorization",
-    //   "Basic YTNhMWM5NWE0ZWFiNjIwY2VkYjI5ZmQzMGJkNTAzM2EyZWU3ZDc4MjNlMTRhNmJkMGI1Mzk0MGRkNzQ3MzExZToyOGViNTc3NjU1M2Q3MzU5MWYzZThjNGNkZmNlOTIxNWViNDdlNTA2ZjQ0ZjM0NmMwMDg2OTMwMDE3NzlhYjZm"
-    // );
-    // xhr.send();
-  }, [groups]);
+      headers: {
+        Authorization: `Basic ${process.env.REACT_APP_PLANNING_CENTER_ENCODED}`,
+      },
+    };
+    axios(options).then((response) => {
+      setGroups(response.data.data);
+      setIsLoading(false);
+    });
+  }, []);
 
   return (
     <div className="small-group">
       <div className="small-group-header">
         <div className="small-group-header-logo">
           <img alt="small groups logo" src={Logo} />
-          <h1>{content.title}</h1>
+          <h1>Small Groups</h1>
         </div>
+        <h3>
+          Live.
+          <br /> Together.
+        </h3>
       </div>
       <div className="small-group-info-sign-up">
-        <div className="small-group-info-sign-up-button-container">
-          <p>{content.ministrySummary}</p>
-        </div>
+        <p>{content.ministrySummary}</p>
+        <div className="small-group-info-sign-up-button-container"></div>
       </div>
       <div className="small-group-map-block">
         {getMobileOperatingSystem() === "iOS" ? <MobileMap /> : <DesktopMap />}
       </div>
 
-      <div>
-        <SmallGroup />
-        <SmallGroup />
-        <SmallGroup />
-        <SmallGroup />
-        <SmallGroup />
-        <SmallGroup />
-        <SmallGroup />
+      <div className="small-groups-list-container">
+        {isLoading ? (
+          <>
+            <p>Loading Services</p>
+            <Spinner color="dark" />
+          </>
+        ) : (
+          <>
+            {groups.map((group) => (
+              <SmallGroupRow
+                image={group.attributes.header_image.thumbnail}
+                name={group.attributes.name}
+                meetingTime={group.attributes.schedule}
+              />
+            ))}
+          </>
+        )}
       </div>
       <StaffInfo
         name={content.leader.name}
