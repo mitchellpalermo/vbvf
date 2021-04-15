5ff7b8aee4639bff8a0c245bf8c1b121976deb35
mitch <mitchellpalermo@icloud.com>
2021-04-14 22:22:10 -0500

    adding church center app section to connect page

--- src/pages/connect-page.js
+++ src/pages/connect-page.js
@@ -1,9 +1,21 @@
 import React from "react";
 import Attendee from "../images/connect/attending_meeting.jpg";
+import iPhones from "../images/connect/Iphones.png";
 import ConnectionStep from "../components/connection-steps";
 import "../css/connect-page.scss";
+import Button from "../components/button";
+import { getMobileOperatingSystem } from "../util";
 
 const ConnectPage = () => {
+  const appStoreLink = () => {
+    if (getMobileOperatingSystem() === "iOS") {
+      return "https://apps.apple.com/us/app/church-center-app/id1357742931";
+    } else if (getMobileOperatingSystem() === "Android") {
+      return "https://play.google.com/store/apps/details?id=com.ministrycentered.churchcenter&hl=en_US";
+    } else {
+      return "https://vbvf.churchcenter.com/home";
+    }
+  };
   return (
     <div className="connect-container">
       <div id="connect-header"></div>
@@ -29,6 +41,22 @@ const ConnectPage = () => {
           />
         </div>
         <img src={Attendee} alt="vbvf member in connection meeting" />
+      </div>
+      <div className="church-center-row">
+        <div className="church-center-row-title-button">
+          <h3>Start connecting now</h3>
+          <p>
+            Check out the church center app to find out what's going on at VBVF.
+            Browse events, explore small groups and more!{" "}
+          </p>
+          <Button
+            title="Get Church Center"
+            size="medium"
+            color="green"
+            link={appStoreLink()}
+          />
+        </div>
+        <img src={iPhones} alt="" />
       </div>
     </div>
   );
