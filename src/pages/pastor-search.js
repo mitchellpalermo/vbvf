c4f30de2fd1d065a27f9e32fec02bc4a533f0d3c
mitch <mitchellpalermo@icloud.com>
2021-12-27 20:05:39 -0600

    updating pastor search page

--- src/pages/pastor-search.js
+++ src/pages/pastor-search.js
@@ -1,75 +1,48 @@
-import React, { useEffect, useState } from "react";
-import { sanity } from "../util/index";
-import PortableText from "@sanity/block-content-to-react";
+import React from "react";
+
 import "../css/pastor-search.scss";
 
-import Banner from "../images/pastor-search/cropped_banner.jpg";
-import { Spinner } from "reactstrap";
+import Video from "../components/video";
 
 export default function PastorSearch() {
-  const pageQuery = `*[_type == "page" && title == "Pastor Search"] {
-    paragraphs,
-    videoId,
-  documents[]{
-    documentTitle,
-      "documentUrl": document.asset->url
-  }
-}`;
-  useEffect(() => {
-    sanity.fetch(pageQuery).then((result) => {
-      setPageContent(result[0]);
-      setPageContentIsLoading(false);
-    });
-    //eslint-disable-next-line
-  }, []);
-
-  const serializers = {
-    //this helps react understand how to present links
-    marks: {
-      link: ({ mark, children }) => {
-        const { href } = mark;
-        return <a href={href}>{children}</a>;
-      },
-      list: (props) => {
-        const { type } = props;
-        const bullet = type === "bullet";
-        if (bullet) {
-          return <ul>{props.children}</ul>;
-        }
-        return <ol>{props.children}</ol>;
-      },
-      listItem: (props) => <li>{props.children}</li>,
-    },
-  };
-
-  const [pageContent, setPageContent] = useState();
-  const [pageContentIsLoading, setPageContentIsLoading] = useState(true);
-
-  const congregationalUpdate = () => {
-    let textToRender = pageContent?.paragraphs?.find((paragraph) =>
-      paragraph.paragraphTitle.includes("Congregational Update")
-    );
-    return (
-      <>
-        <h1>{textToRender.paragraphTitle}</h1>
-        <PortableText
-          blocks={textToRender.bodyText}
-          serializers={serializers}
-        />
-      </>
-    );
-  };
-
   return (
     <div className="pastor-search">
-      <img src={Banner} alt="" />
-
       <div className="pastor-search-container">
-        {pageContentIsLoading ? (
-          <Spinner color="dark" />
-        ) : (
-          <div className="pastor-search-content">{congregationalUpdate()}</div>
-        )}
+        <div className="pastor-search-content">
+          <h1>Pastoral Search Update</h1>
+          <p>
+            After much prayer and discussion, Pastor Tony Caffey has been called
+            as the new Senior Pastor of Verse by Verse by Fellowship. The Search
+            Committee and the Elders having been working for almost a year to
+            find God’s person to serve. We believe God led us to Tony and we
+            praise God for his faithfulness.
+          </p>
+
+          <p>
+            Tony is married to Sanja, and they have one son Alastair. Tony has
+            been the Senior Pastor at Harvest Church in Decatur Illinois for 14
+            years. Tony grew up in Austin, Texas where his parents still live.
+            He attended college in East Texas where he met his wife Sanja. They
+            moved to Chicago for seminary. He has a Master of Divinity degree
+            from Moody Bible Institute and a PhD in Old Testament studies from
+            Trinity Evangelical Divinity School.
+          </p>
+          <Video videoId="660488765" />
+          <div className="pastor-search-content-prayer">
+            <h4>Tony has asked us to pray for three things.</h4>
+            <ol>
+              <li>Pray his current home will be sold quickly.</li>
+              <li>Pray for Harvest Decatur and their transition.</li>
+              <li>Pray for the future of VBVF.</li>
+            </ol>
+          </div>
+          <p>
+            We anticipate the Caffey family will be here by the end of February.
+            Please pray God blesses this transition as we continue the
+            dedication to God’s word that is embedded in our name, Verse by
+            Verse Fellowship.
+          </p>
+        </div>
       </div>
     </div>
   );
