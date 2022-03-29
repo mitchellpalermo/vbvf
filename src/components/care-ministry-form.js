9f7672b5c1ed0b1b614663e0c3d4a866559f1cf9
mitch <mitchellpalermo@icloud.com>
2022-03-29 13:32:20 -0500

    adding back form validation

--- src/components/care-ministry-form.js
+++ src/components/care-ministry-form.js
@@ -1,7 +1,7 @@
-import { Formik, Field, Form } from "formik";
+import { Formik, Field, Form, ErrorMessage } from "formik";
 import React, { useState } from "react";
 import "../css/care-ministry-form.scss";
-// import * as Yup from "yup";
+import * as Yup from "yup";
 import { motion } from "framer-motion";
 import Button from "./button";
 
@@ -18,8 +18,8 @@ export default function CareMinistryForm({ closeFunc }) {
       fill: "rgba(255, 255, 255, 1)",
     },
   };
-  // const phoneRegExp =
-  //   /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+  const phoneRegExp =
+    /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
   const encode = (data) => {
     return Object.keys(data)
@@ -56,8 +56,7 @@ export default function CareMinistryForm({ closeFunc }) {
       ) : (
         <Formik
           initialValues={{
-            firstName: "",
-            lastName: "",
+            fullName: "",
             phoneNumber: "",
             email: "",
             contactOptions: [],
@@ -65,6 +64,34 @@ export default function CareMinistryForm({ closeFunc }) {
             counselor: "",
             contactPermission: "No",
           }}
+          validationSchema={Yup.object({
+            fullName: Yup.string()
+              .max(20, "Must be 20 characters or less.")
+              .required("Name Required"),
+            //only required when contact options preference contains phone
+            phoneNumber: Yup.string().when("contactOptions", {
+              is: (val) => val.includes("Phone") === true,
+              then: Yup.string()
+                .required("Phone Number Required")
+                .matches(phoneRegExp, "Please enter a valid phone number"),
+              otherwise: Yup.string().matches(
+                phoneRegExp,
+                "Please enter a valid phone number"
+              ),
+            }),
+            //only required when contact options preference contains email
+            email: Yup.string().when("contactOptions", {
+              is: (val) => val.includes("Email") === true,
+              then: Yup.string()
+                .email("Invalid email address")
+                .required("Email Address Required"),
+              otherwise: Yup.string().email("Invalid email address"),
+            }),
+            contactOptions: Yup.array().min(
+              1,
+              "Please Select a Contact Preference"
+            ),
+          })}
           onSubmit={(values, actions) => {
             fetch("/", {
               method: "POST",
@@ -93,11 +120,25 @@ export default function CareMinistryForm({ closeFunc }) {
 
               <label name="fullName">Full Name</label>
               <Field className="text-field" name="fullName" autoFocus />
+              <ErrorMessage
+                component="div"
+                className="contact-error-message"
+                name="fullName"
+              />
               <label name="email">Email Address</label>
               <Field className="text-field" name="email" type="email" />
+              <ErrorMessage
+                component="div"
+                className="contact-error-message"
+                name="email"
+              />
               <label name="phoneNumber">Phone Number</label>
               <Field className="text-field" name="phoneNumber" type="tel" />
-
+              <ErrorMessage
+                component="div"
+                className="contact-error-message"
+                name="phoneNumber"
+              />
               <label name="contactOptions">Preferred method of contact</label>
               <span
                 role="group"
@@ -113,6 +154,11 @@ export default function CareMinistryForm({ closeFunc }) {
                   Email
                 </label>
               </span>
+              <ErrorMessage
+                component="div"
+                className="contact-error-message"
+                name="contactOptions"
+              />
               <span className="previous-counseling">
                 Are you currently receiving or have you previously received
                 counseling from VBVF?
